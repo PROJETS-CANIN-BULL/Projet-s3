f3baa9e84b6b2b7514c0e5be8b7c4da4303ea8db
Pernot Antoine <antoine.pernot@et.umontpellier.fr>
2021-10-07 15:45:46 +0200

    ajout du css et modif Connexion.php

--- view/Connexion.php
+++ view/Connexion.php
@@ -3,27 +3,32 @@
 <head>
     <meta charset="utf-8" />
     <title> Bull's Friends Association </title>
+    <link rel="stylesheet" href="../css/formulaire.css">
 </head>
 <body>
 
-<form method="get" action="../controller/routeur.php">
+<form id="formulaire" method="get" action="../controller/routeur.php">
     <!--<form method="post" action="created.php"> -->
     <fieldset>
         <legend>Se connecter</legend>
         <p>
 
 
-            <label for="id">Identifiant</label> :
+            <label for="id">Identifiant</label>
             <input type="text" placeholder="Identifiant" name="id" id="id" required/>
         </p>
         <p>
-            <label for="password">Mot de Passe</label> :
+            <label for="password">Mot de Passe</label>
             <input type="text"placeholder="Mot de passe" name="password" id="password" required>
         </p>
         <p>
             <input type="submit" value="Envoyer" />
             <input type='hidden' name='action' value='connexion'>
 
+        </p>
+
+        <p>
+            <a href="account_creation.php">Créer un compte</a>
         </p>
     </fieldset>
 </form>
---
+++ view/account_creation.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title> Bull's Friends Association </title>
+    <link rel="stylesheet" href="../css/formulaire.css">
+</head>
+<body>
+<form id="formulaire" method="get" action="../controller/routeur.php">
+<!--<form method="post" action="created.php"> -->
+<fieldset>
+    <legend>Création de compte</legend>
+    <p>
+        <label for="id">Identifiant</label>
+        <input type="text" placeholder="Identifiant" name="id" id="id" required/>
+    </p>
+    <p>
+        <label for="password">Mot de Passe</label>
+        <input type="text"placeholder="Mot de passe" name="password" id="password" required>
+    </p>
+    <p>
+        <input type="submit" value="Envoyer" />
+        <input type='hidden' name='action' value='connexion'>
+
+    </p>
+</body>
+</html>
