b3a4abdd012d8b54e389abe25560d8b340f60d0c
fanny-petitjean <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>
2021-10-08 21:48:50 +0200

    presentation

--- view/FAQ.php
+++ view/FAQ.php
@@ -6,10 +6,16 @@
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <link href="../css/commun.css" rel="stylesheet" type="text/css">
     <link href="../css/FAQ.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
 </head>
 <body>
-    <h2> FAQ</h2>
+
     <header>
+      <div class ="row">
+    <div class="container text-center">
+      <h2>Bull's Friends</h2>
+    </div>
+    <div class="container">
           <nav>
         <ul id="menu">
             <li><a href="../view/accueil.php">Logo</a></li>
@@ -24,8 +30,23 @@
 
     </header>
     <main>
-    	<article>
-    	</article>
+      <div class="container-fluid red">
+            <div class="container text-center">
+        <h2> FAQ</h2>
+      </div>
+      <div class="container-fluid blue">
+        <div class="row">
+          <article class="col-2">
+            <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.gkqdfhgdqf dfglkjjdfhgmkdhfghd gfqkgkdfmghdfmqogqdf gimqdongqdfingqdfoîgqd gdfmiingdfgndio</p>
+
+          </article>
+          <article class="col-10">
+            <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.</p>
+          </article>
+
+        </div>
+      </div>
+    </div>
 
     </main>
      <footer>
