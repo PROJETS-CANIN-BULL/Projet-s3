aea6b47bbdc19a11d511006bf4e434df166f3056
Petitjean Fanny <fanny.petitjean@etu.umontpellier.fr>
2021-10-12 12:05:05 +0200

    d

--- view/Frais.php
+++ view/Frais.php
@@ -39,8 +39,14 @@
           <div class="container-fluid blue">
           <div class="row">
             <article class="col-2">
-              <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel terna, pecudum ritu inertium trucidantur.Quare talis improborum consensio non modo excusatione amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam futura, quam qualis hodie sit.Haec igitur Epicuri non probo, inquam. De cetero vellem equidem aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis. quamquam te quidem video minime esse deterritum.His cognitis Gallus ut serpens adpetitus telo vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri fortes mihi periclitanti vobiscum.gkqdfhgdqf dfglkjjdfhgmkdhfghd gfqkgkdfmghdfmqogqdf gimqdongqdfingqdfoîgqd gdfmiingdfgndio</p>
-
+              <div>
+              <p>Cliquez sur le bouton ci-dessous pour ajouter un Portégé: </p>
+              <button class="btn" type="button">Ajouter un Protégé</button>
+            </div>
+            <div>
+              <p>Cliquez sur le bouton ci-dessous pour ajouter une Facture : </p>
+              <button class="btn" type="button">Ajouter une Facture</button>
+            </div>
             </article>
             <article class="col-10">
               <?php
