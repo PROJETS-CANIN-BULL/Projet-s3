44d0755be4e8014481620b7e86e206b6b791adb6
Marion <marion.randrianasolo@etu.umontpellier.fr>
2021-11-22 17:59:24 +0100

    update

--- view/FAQ.php
+++ view/FAQ.php
@@ -19,20 +19,77 @@
 
                 </article>
                 <article class="col-10">
-                    <p>Quam ob rem circumspecta cautela observatum est deinceps et cum edita montium petere coeperint
-                        grassatores, loci iniquitati milites cedunt. ubi autem in planitie potuerint reperiri, quod
-                        contingit adsidue, nec exsertare lacertos nec crispare permissi tela, quae vehunt bina vel
-                        terna, pecudum ritu inertium trucidantur.Quare talis improborum consensio non modo excusatione
-                        amicitiae tegenda non est sed potius supplicio omni vindicanda est, ut ne quis concessum putet
-                        amicum vel bellum patriae inferentem sequi; quod quidem, ut res ire coepit, haud scio an
-                        aliquando futurum sit. Mihi autem non minori curae est, qualis res publica post mortem meam
-                        futura, quam qualis hodie sit.Haec igitur Epicuri non probo, inquam. De cetero vellem equidem
-                        aut ipse doctrinis fuisset instructior est enim, quod tibi ita videri necesse est, non satis
-                        politus iis artibus, quas qui tenent, eruditi appellantur aut ne deterruisset alios a studiis.
-                        quamquam te quidem video minime esse deterritum.His cognitis Gallus ut serpens adpetitus telo
-                        vel saxo iamque spes extremas opperiens et succurrens saluti suae quavis ratione colligi omnes
-                        iussit armatos et cum starent attoniti, districta dentium acie stridens adeste inquit viri
-                        fortes mihi periclitanti vobiscum.</p>
+                    <div id="content">
+                        <ul>
+                            <li>
+                                <div class="cible" id="q1"></div>
+                                <div class="question">
+                                    <a href="#q1"><h3>Quel est notre lieu d'activité ? </h3></a>
+                                </div>
+                                <div class="reponse">
+                                    <p>
+                                        Nous opérons en <strong>France</strong> et en <strong>Belgique</strong>.
+                                    </p>
+
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cible" id="q3"></div>
+                                <div class="question">
+                                    <a href="#q3"><h3>Comment adopter un chien ?</h3></a>
+                                </div>
+                                <div class="reponse">
+                                    <p>
+                                        Pour adopter un chien, vous pouvez remplir le <strong>formulaire</strong> du protégé que vous souhaitez adopter sur la page <strong><a href="index.php?controller=Chien&action=Adopter">'À adopter'</a></strong>
+                                        afin de candidater. Nous évaluerons votre demande et prendrons contact avec vous
+                                        ensuite.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cible" id="q4"></div>
+                                <div class="question">
+                                    <a href="#q4"><h3>Vous souhaitez être famille d'accueil bénévole pour les protégés
+                                            ? </h3></a>
+                                </div>
+                                <div class="reponse">
+                                    <p>
+                                        Vous pouvez remplir le <strong><a href="index.php?controller=FamilleAccueil&action=formulaireFamilleAccueil">formulaire d'accueil</a></strong> afin de candidater en tant que
+                                        famille d'accueil bénévole en attendant que le protégé trouve une famille.
+                                        Vous retrouverez toutes les modalités sur le document généré par le formulaire.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cible" id="q5"></div>
+                                <div class="question">
+                                    <a href="#q5">
+                                        <h3>L'adoption est-elle payante ?</h3>
+                                    </a>
+                                </div>
+                                <div class="reponse">
+                                    <p>
+                                        Il ne s'agit pas d'un achat, mais l'association Bull's Friends vous demande une
+                                        <strong>cotisation</strong> pour l'adoption du protégé.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cible" id="q2"></div>
+                                <div class="question"><a href="#q2"><h3>Comment pouvez-vous nous contacter ?</h3></a>
+                                </div>
+                                <div class="reponse">
+                                    <p>
+                                        Vous pouvez nous contacter via la <strong><a href="index.php?action=Contact">page contact</a></strong>, ou nous contacter sur <strong><a href="https://www.facebook.com/Bullsfriendsassociation/">Facebook</a></strong>.
+                                    </p>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="cible" id="quit"></div>
+                                <a href="#quit"><h3>Fermer</h3></a>
+                            </li>
+                        </ul>
+                    </div>
                 </article>
 
             </div>
